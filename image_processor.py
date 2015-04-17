f0c6fb8bd029f477920808a25f7144b11d7c6754
lockys <locky4567@gmail.com>
2015-04-17 16:03:10 +0800

    indent bug fixed

--- image_processor.py
+++ image_processor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import boto
 import json
 import time
@@ -17,6 +18,8 @@ from boto.s3.key import Key
 ##########################################################
 # Connect to SQS and poll for messages
 ##########################################################
+
+
 def main(argv=None):
 	# Handle command-line arguments for AWS credentials and resource names
 	parser = argparse.ArgumentParser(description='Process AWS resources and credentials.')
@@ -47,7 +50,7 @@ def main(argv=None):
 	# Get S3 bucket, create if none supplied
 	s3_output_bucket = args.s3_output_bucket
 	if s3_output_bucket == "":
-	  s3_output_bucket = create_s3_output_bucket(s3_output_bucket, s3_endpoint, region_name)
+		s3_output_bucket = create_s3_output_bucket(s3_output_bucket, s3_endpoint, region_name)
 
 	info_message('Retrieving jobs from queue %s. Processed images will be stored in %s and a message placed in queue %s' % (input_queue_name, s3_output_bucket, output_queue_name))
 
@@ -110,6 +113,8 @@ def main(argv=None):
 ##############################################################################
 # Process a newline-delimited list of URls
 ##############################################################################
+
+
 def process_message(message, s3_output_bucket, s3_endpoint, job_id):
 	try:
 		output_dir = "/home/ec2-user/jobs/%s/" % (job_id)
@@ -122,9 +127,9 @@ def process_message(message, s3_output_bucket, s3_endpoint, job_id):
 		output_image_path = output_dir + output_image_name
 		# Invoke ImageMagick to create a montage
 		os.system("montage -size 400x400 null: %s*.* null: -thumbnail 400x400 -bordercolor white -background black +polaroid -resize 80%% -gravity center -background black -geometry -10+2  -tile x1 %s" % (output_dir, output_image_path))
-        # Write the resulting image to s3
-        os.system("convert %s -set colorspace Gray -separate -average %s", (output_image_path, output_image_path))
-        output_url = write_image_to_s3(output_image_path, output_image_name, s3_output_bucket, s3_endpoint)
+		# Write the resulting image to s3
+		os.system("convert %s -set colorspace Gray -separate -average %s", (output_image_path, output_image_path))
+		output_url = write_image_to_s3(output_image_path, output_image_name, s3_output_bucket, s3_endpoint)
 		# Return the output url
 		return output_url
 	except:
@@ -133,6 +138,9 @@ def process_message(message, s3_output_bucket, s3_endpoint, job_id):
 ##############################################################################
 # Write the result of a job to the output queue
 ##############################################################################
+#
+
+
 def write_output_message(message, output_queue):
 	m = RawMessage()
 	m.set_body(message)
@@ -140,6 +148,9 @@ def write_output_message(message, output_queue):
 ##############################################################################
 # Write an image to S3
 ##############################################################################
+#
+
+
 def write_image_to_s3(path, file_name, s3_output_bucket, s3_endpoint):
 	# Connect to S3 and get the output bucket
 	s3 = boto.connect_s3(host=s3_endpoint)
@@ -156,6 +167,8 @@ def write_image_to_s3(path, file_name, s3_output_bucket, s3_endpoint):
 ##############################################################################
 # Verify S3 bucket, create it if required
 ##############################################################################
+
+
 def create_s3_output_bucket(s3_output_bucket, s3_endpoint, region_name):
 
 	# Connect to S3
@@ -163,18 +176,21 @@ def create_s3_output_bucket(s3_output_bucket, s3_endpoint, region_name):
 	# Find any existing buckets starting with 'image-bucket'
 	buckets = [bucket.name for bucket in s3.get_all_buckets() if bucket.name.startswith('cp04-image-bucket')]
 
-    if len(buckets) > 0:
-        return buckets[0]
+	if len(buckets) > 0:
+		return buckets[0]
 
-    # No buckets, so create one for them
+	# No buckets, so create one for them
 	name = 'cp04-image-bucket-' + str(uuid.uuid4())
 	s3.create_bucket(name, location=region_name)
 	return name
 ##############################################################################
 # Use logging class to log simple info messages
 ##############################################################################
+
+
 def info_message(message):
 	logger.info(message)
+
 
 def error_message(message):
 	logger.error(message)
@@ -182,22 +198,24 @@ def error_message(message):
 ##############################################################################
 # Generic stirng logging
 ##############################################################################
+
+
 class Logger:
 	def __init__(self):
-		#self.stream = StringIO.StringIO()
-		#self.stream_handler = logging.StreamHandler(self.stream)
 		self.file_handler = logging.FileHandler('/home/ec2-user/image_processor.log')
 		self.log = logging.getLogger('image-processor')
 		self.log.setLevel(logging.INFO)
 		for handler in self.log.handlers:
 			self.log.removeHandler(handler)
 		self.log.addHandler(self.file_handler)
+
 	def info(self, message):
 		self.log.info(message)
+
 	def error(self, message):
 		self.log.error(message)
 
 logger = Logger()
 
 if __name__ == "__main__":
-    sys.exit(main())
+	sys.exit(main())
